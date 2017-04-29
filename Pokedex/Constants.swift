dfd78729876b57dbf8be0cd3be4267043e3b22d3
Jamel Reid <jreid@qualityworkscg.com>
2017-04-29 18:46:43 -0500

    installed alamofire and used to to pull basic pokemon information from the pokemon api

---
+++ Pokedex/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  Pokedex
+//
+//  Created by Jamel Reid  on 4/29/17.
+//  Copyright © 2017 Jamel Reid . All rights reserved.
+//
+
+import Foundation
+
+
+let URL_BASE = "http://pokeapi.co"
+let URL_POKEMON = "/api/v1/pokemon/"
+
+typealias DownloadComplete = () -> ()
--- Pokedex/PokemonDetailVC.swift
+++ Pokedex/PokemonDetailVC.swift
@@ -11,12 +11,44 @@ import UIKit
 class PokemonDetailVC: UIViewController {
     
     @IBOutlet weak var nameLbl: UILabel!
+    @IBOutlet weak var mainImage: UIImageView!
+    @IBOutlet weak var descriptionLbl: UILabel!
+    @IBOutlet weak var typeLbl: UIStackView!
+    @IBOutlet weak var defenseLbl: UILabel!
+    @IBOutlet weak var heightLbl: UILabel!
+    @IBOutlet weak var pokedexLbl: UILabel!
+    @IBOutlet weak var weightLbl: UILabel!
+    @IBOutlet weak var attackLbl: UILabel!
+    @IBOutlet weak var currentEvoImage: UIImageView!
+    @IBOutlet weak var nextEvoImage: UIImageView!
+    @IBOutlet weak var evoLbl: UILabel!
+    
+    
     var pokemon: Pokemon!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         nameLbl.text = pokemon.name
+        
+        pokemon.downloadPokemonDetails {
+            
+            // Will be called after the network call is complete!
+            
+            print("================>")
+            
+            self.updateUI()
+        }
+    }
+    
+    func updateUI() {
+        
+        attackLbl.text = pokemon.attack
+        defenseLbl.text = pokemon.defense
+        heightLbl.text = pokemon.height
+        weightLbl.text = pokemon.weight
+        pokedexLbl.text = "\(pokemon.pokedexId)"
+        mainImage.image = UIImage(named: "\(pokemon.pokedexId).png")
     }
 
     override func didReceiveMemoryWarning() {
@@ -24,5 +56,8 @@ class PokemonDetailVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func backButtonPressed(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
 
 }
