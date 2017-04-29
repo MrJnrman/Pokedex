89780076c9b90f5993584837acb4bdda8c7400de
Jamel Reid <jreid@qualityworkscg.com>
2017-04-29 14:56:31 -0500

    updated UI to display all pokemon and to play music

---
+++ Pokedex/PokeCell.swift
@@ -0,0 +1,34 @@
+//
+//  PokeCell.swift
+//  Pokedex
+//
+//  Created by Jamel Reid  on 4/29/17.
+//  Copyright © 2017 Jamel Reid . All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbImage: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    // used to modify UI at the layer level
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        layer.cornerRadius = 5.0
+    }
+    
+    func configureCell(_ pokemon: Pokemon) {
+        
+        self.pokemon = pokemon
+        
+        nameLbl.text = self.pokemon.name.capitalized
+        thumbImage.image = UIImage(named: "\(self.pokemon.pokedexId)")
+        
+    }
+    
+}
---
+++ Pokedex/PokemonVC.swift
@@ -0,0 +1,116 @@
+//
+//  ViewController.swift
+//  Pokedex
+//
+//  Created by Jamel Reid  on 4/29/17.
+//  Copyright © 2017 Jamel Reid . All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class PokemonVC: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    @IBOutlet weak var collection: UICollectionView!
+    
+    var pokemon = [Pokemon]()
+    
+    var musicPlayer: AVAudioPlayer!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+//        let charmander = Pokemon(name: "Charmander", pokedexId: 4)
+        collection.dataSource = self
+        collection.delegate = self
+        
+        parsePokemonCSV()
+        initAudio()
+    }
+    
+    func initAudio() {
+        
+        let path = Bundle.main.path(forResource: "music", ofType: "mp3")
+        
+        do {
+            
+            musicPlayer = try AVAudioPlayer(contentsOf: URL(string: path!)!)
+            musicPlayer.prepareToPlay()
+            // continous loop
+            musicPlayer.numberOfLoops = -1
+            musicPlayer.play()
+            
+        } catch let err as NSError {
+            print(err.debugDescription)
+        }
+    }
+    
+    func parsePokemonCSV() {
+        
+        let path = Bundle.main.path(forResource: "pokemon", ofType: "csv")!
+        
+        do {
+            let csv = try CSV(contentsOfURL: path)
+            let rows = csv.rows
+//            print(rows)
+            
+            for row in rows {
+                let pokeId = Int(row["id"]!)
+                let name = row["identifier"]!
+                
+                let poke = Pokemon(name: name, pokedexId: pokeId!)
+                pokemon.append(poke)
+            }
+            
+            print(pokemon)
+        } catch let err as NSError {
+            print(err.debugDescription)
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PokeCell", for: indexPath) as? PokeCell {
+            
+            let poke = pokemon[indexPath.row]
+            cell.configureCell(poke)
+            
+            return cell
+        } else {
+            return UICollectionViewCell()
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return pokemon.count
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        return CGSize(width: 85, height: 85)
+    }
+
+    @IBAction func MusicButtonPressed(_ sender: UIButton) {
+        
+        // sender is used to change the buttons transparency
+        
+        if musicPlayer.isPlaying {
+            musicPlayer.pause()
+            sender.alpha = 0.2
+        } else {
+            musicPlayer.play()
+            sender.alpha = 1.0
+        }
+        
+    }
+
+}
+
